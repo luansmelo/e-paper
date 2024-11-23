4124a0d57f3c09756f81c0829d1b5fac3652064e
luansmelo <luan_decruz19@hotmail.com>
2024-11-23 10:53:11 -0300

    refactor: validate the existence of the document and remove old document from the bucket

--- src/application/usecases/upload-file.usecase.ts
+++ src/application/usecases/upload-file.usecase.ts
@@ -1,24 +1,43 @@
-import { PutObjectCommand } from "@aws-sdk/client-s3";
-import { Injectable } from "@nestjs/common";
+import { DeleteObjectCommand, PutObjectCommand } from "@aws-sdk/client-s3";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { s3 } from "../../config/multer-s3.config"
 import { ConfigService } from "@nestjs/config";
 import { v4 as uuidv4 } from 'uuid';
 import { UpdateDocumentByIdRepository } from "@/domain/repositories/update-document";
 import { Document } from "@/domain/entities/document.entity";
+import { LoadDocumentByIdRepository } from "@/domain/repositories/load-document-by-id";
 
 @Injectable()
 export class UploadFileUseCase {
     constructor(
         private readonly config: ConfigService,
         private readonly update: UpdateDocumentByIdRepository,
+        private readonly loadDocument: LoadDocumentByIdRepository,
     ) { }
 
     async execute(documentId: string, file: Express.Multer.File): Promise<Document> {
-        const bucket = this.config.get<string>('MINIO_BUCKET');
-        const uuid = uuidv4();
-        const fileExtension = file.originalname.split('.').pop();
-        const filename = `${uuid}-${file.originalname}`;
+        const document = await this.loadDocument.loadById(documentId)
 
+        if (!document) {
+            throw new NotFoundException("Document not found");
+        }
+        
+        const bucket = this.config.get<string>('MINIO_BUCKET');
+
+        if (document.fileUrl) {
+            const oldFileKey = document.fileUrl.split(`${bucket}/`)[1];
+
+            const deleteObjectCommand = new DeleteObjectCommand({
+                Bucket: process.env.MINIO_BUCKET as string,
+                Key: oldFileKey,
+            });
+    
+            await s3.send(deleteObjectCommand);
+        }
+
+        const uuid = uuidv4();
+        const fileExtension = file.mimetype.split('/')[1];
+        const filename = `${uuid}-${file.originalname}`;
         const fileKey = `uploads/${filename}.${fileExtension}`;
 
         const command = new PutObjectCommand({
@@ -33,7 +52,7 @@ export class UploadFileUseCase {
 
         return await this.update.updateById(documentId, {
             fileUrl,
-            name: filename,
+            name: file.originalname,
         });
     }
 
